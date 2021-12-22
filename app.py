c612308a8b0f89f79748a3d10eed7c0facd2a0c6
Cathleen <cataspena@hotmail.com>
2021-12-21 23:45:03 -0800

    Update 3
    create app template and blank html homepage

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('models/rfr_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    return
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    return
+

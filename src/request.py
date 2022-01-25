48331d4effc29442555fe4504dae6e86ad322426
Cathleen <cataspena@hotmail.com>
2022-01-24 20:58:31 -0800

    Update 9
    Delete deployment attempt #1

--- src/request.py
+++ src/request.py
@@ -2,20 +2,28 @@ import requests
 
 url = 'http://127.0.0.1:5000/'
 
-body = {"id": "[3,2,1000,1500,5,-38,-58,'argentina','buenos aires','house','for rent']"}
-#{
+########################################################
+# DATA INFORMATION IS ENTERED IN THE ORDER AS FOLLOWS: #
+########################################################
+
+# {
 #    "bedrooms":3,
-#    "bathrooms":2, 
-#    "surface_total":1000, 
-#    "surface_covered":1500, 
-#    "rooms":5, 
-#    "lat":-38, 
-#    "lon":-58, 
-#    "l1":"argentina", 
-#    "l2":"buenos aires", 
-#    "property_type":"house", 
-#    "operation_type":"for rent"
-#}
+#    "bathrooms":2,
+#    "surface_total":1000,
+#    "surface_covered":1500,
+#    "rooms":5,
+#    "lat":-38,
+#    "lon":-58,
+#    "l1":"argentina",
+#    "l2":"buenos aires",
+#    "property_type":"house",
+#    "operation_type":"for rent"   # must be either 'for rent' , 'for sale' , or 'for sublease'
+# }
+
+# COMES PREPOPULATED BUT FEEL FREE TO ENTER ANY INFORMATION YOU'D LIKE FOR YOUR HOUSE PRICE PREDICTION
+
+body = {
+    "id": "[3,2,1000,1500,5,-38,-58,'argentina','buenos aires','house','for rent']"}
 
 response = requests.post(url, data=body)
 
--- src/run.py
+++ src/run.py
@@ -3,7 +3,6 @@
 ###################
 
 import pandas as pd
-import numpy as np
 import pickle
 import sys
 
@@ -16,48 +15,52 @@ warnings.filterwarnings("ignore")
 # CREATE FUNCTIONS #
 ####################
 
+
 def train_model():
-    
+
     ### LOAD DATA ###
-    df = pd.read_csv('data/argentina_final.csv')
-    df.drop(['Unnamed: 0', 'price', 'start_date', 'end_date', 'created_on'], axis=1, inplace=True)
-    
+    df = pd.read_csv('../data/argentina_final.csv')
+    df.drop(['Unnamed: 0', 'price', 'start_date',
+            'end_date', 'created_on'], axis=1, inplace=True)
+
     ### SPLIT DATA ###
     x = df.drop(['price'], axis=1)
     y = df.price
-    
+
     ### FIT MODEL ###
+    print("fitting model...")
     rfr_final = RandomForestRegressor(n_jobs=-1)
     rfr_final.fit(x, y)
-    
+
     ### PICKLE MODEL ###
     filename = 'models/rfr_model.pkl'
     pickle.dump(rfr_final, open(filename, 'wb'))
-    
+
     ### SAVE COLUMNS MODEL USES ###
     model_cols = list(x.columns)
     filename2 = 'models/rfr_model_columns.pkl'
     pickle.dump(model_cols, open(filename2, 'wb'))
-    
+
     return "Model Trained and Saved"
 
+
 def predict(data):
-    
+
     print('loading data...')
     ### LOAD DATA ###
     X_test = pd.read_csv(data)
-    
+
     ### LOAD MODEL ###
     filename = 'models/rfr_model.pkl'
     loaded_model = pickle.load(open(filename, 'rb'))
-    
+
     print('selecting columns...')
     ### LOAD COLUMNS ###
     filename2 = 'models/rfr_model_columns.pkl'
     loaded_cols = pickle.load(open(filename2, 'rb'))
     #X_test = X_test[loaded_cols]
     X_test = X_test[X_test.columns[X_test.columns.isin(loaded_cols)]]
-    
+
     print('making predictions...')
     ### MAKE PREDICTIONS ###
     predictions = loaded_model.predict(X_test)
@@ -68,12 +71,9 @@ def predict(data):
 
 
 if __name__ == "__main__":
-    if sys.argv[2] == 'True':
-        train_model()
-    predict(sys.argv[1])
 
+    train_model()
 
-
-
-
-
+# If user wants to predict a csv of data, enter csv location as second command line argument.
+    if sys.argv[1] == 'True':
+        predict(sys.argv[1])
